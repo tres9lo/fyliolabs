b57e2d695730645e514ad8782a616a5bbeb4af9d
tres9lo <bikotreslo.99@gmail.com>
2026-05-19 22:43:11 +0200

    ui fixed and looking good

--- src/components/file/file-list.tsx
+++ src/components/file/file-list.tsx
@@ -36,7 +36,7 @@ export function FileList({
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))] gap-5">
       {files.map((file) => (
         <FileCard 
           key={file.id} 
